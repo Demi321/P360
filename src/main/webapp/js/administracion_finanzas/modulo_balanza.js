b8cc27940cad1bc2fd56e6929dc8c5c070f7a78f
fernando-ag <fernando.aguilar@globalcorporation.cc>
2020-12-18 17:57:36 -0600

    js y css para modulos de finanzas

---
+++ src/main/webapp/js/administracion_finanzas/modulo_balanza.js
@@ -0,0 +1,7 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+agregar_menu("Balanza", '<i class="fas fa-hand-holding-usd"></i>',"Administración y Finanzas");
