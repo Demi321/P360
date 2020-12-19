4616d997829b3d98f0ed04eed50f23cb29dfe366
fernando-ag <fernando.aguilar@globalcorporation.cc>
2020-12-18 18:26:47 -0600

    Se agregaron modulos para geodatos

---
+++ src/main/webapp/js/Empresa/modulo_geodatos.js
@@ -0,0 +1,11 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+agregar_menu("Geodatos Empresa",'<i class="fas fa-building"></i>',"Geodatos");
+agregar_menu("Infraestructura",'<i class="fas fa-map-marked-alt"></i>',"Geodatos");
+agregar_menu("Incidentes",'<i class="fas fa-car-crash"></i>',"Geodatos");
+agregar_menu("Información COVID-19",'<i class="fas fa-vest-patches"></i>',"Geodatos");
+agregar_menu("Análisis Estadístico",'<i class="fas fa-chart-bar"></i>',"Geodatos");
