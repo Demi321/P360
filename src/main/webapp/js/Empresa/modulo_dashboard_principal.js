1ec973923d637f7add4fdb5bd629474bc4fda6ba
Moises-Juarez <moises.juarez@globalcorporation.cc>
2021-01-13 10:01:38 -0600

    personalizando rama para usuario Moises

---
+++ src/main/webapp/js/Empresa/modulo_dashboard_principal.js
@@ -0,0 +1,9 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+agregar_menu("Dashboard Principal",'<i class="fas fa-undo-alt"></i>','Moisés');
+
+console.log("Dashboard Principal ");
