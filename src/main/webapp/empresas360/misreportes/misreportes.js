27359a53782c56a075eb340e49ff17a9bd689ca0
alfredoperaltag <alfredo08peralta@gmail.com>
2021-02-17 17:38:49 -0600

    validacion de vista tutorial

--- src/main/webapp/empresas360/misreportes/misreportes.js
+++ src/main/webapp/empresas360/misreportes/misreportes.js
@@ -11,4 +11,14 @@ const init_misreportes = (json) => {
     let tipo_usuario = json.tipo_usuario;
     let tipo_servicio = json.tipo_servicio;
     let tipo_area = json.tipo_area;
+    
+    //el id del json de entrada corresponde al id_menu
+    //consultar si el tutorial de la vista ya fue consultada 
+    RequestPOST("",{
+        "id360":perfil_usuario.id360,
+        "id_menu":json.id
+    }).then((response)=>{
+        console.log("Estatus de la vista tutorial de mis reportes");
+        console.log(response);
+    });
 };
